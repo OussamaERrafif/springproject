d375784c4d8c5e5f39a068e922703b1e536754f1
OussamaERrafif <oussamaerra2002@gmail.com>
2024-03-28 09:23:46 +0000

    new

--- src/main/java/com/example/miniproject/controllers/voyagePlanifieController.java
+++ src/main/java/com/example/miniproject/controllers/voyagePlanifieController.java
@@ -2,12 +2,11 @@
 package com.example.miniproject.controllers;
 
 import com.example.miniproject.entities.VoyagePlanifie;
-import com.example.miniproject.services.VoyagePlanifieService;
+import com.example.miniproject.services.VoyagePlanifieServiceIMP;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,18 +16,38 @@ import java.util.Optional;
 public class voyagePlanifieController {
 
     @Autowired
-    private VoyagePlanifieService voyagePlanifieService;
+    private VoyagePlanifieServiceIMP voyagePlanifieService;
 
-    @GetMapping(produces = "application/json")
+    @GetMapping(path="/{id}", produces = "application/json")
     public Optional<VoyagePlanifie> getVoyageById(@PathVariable Long id) {
         return voyagePlanifieService.getVoyageById(id);
     }
 
-    @GetMapping(path="/{id}", produces = "application/json")
+
+    @GetMapping(produces = "application/json")
     public List<VoyagePlanifie> getAllVoyages() {
         return voyagePlanifieService.getAllVoyages();
     }
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public ResponseEntity<Object> addVoyage(@RequestBody VoyagePlanifie voyagePlanifie) {
+        VoyagePlanifie addedVoyage = voyagePlanifieService.add(voyagePlanifie);
+        if (addedVoyage != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("Voyage added successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add voyage.");
+        }
+    }
 
 
+    @DeleteMapping(path="/{id}", produces = "application/json")
+    public ResponseEntity<String> deleteVoyageById(@PathVariable Long id) {
+        Optional<VoyagePlanifie> voyage = voyagePlanifieService.getVoyageById(id);
+        if (voyage.isPresent()) {
+            voyagePlanifieService.deleteById(id);
+            return ResponseEntity.ok("Voyage with ID " + id + " has been deleted successfully.");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
